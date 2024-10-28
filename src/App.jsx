a9012b2cd13526fb2505570b3f69bdced612116c
EngineerMoore <alexiashalise@gmail.com>
2024-10-28 16:36:15 -0500

    rendered puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { puppyList } from './data'
+import { useState } from 'react'
+
 function App() {
+  console.log(`puppies:`, puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
   return (
     <>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
